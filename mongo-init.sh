55a7178d123c38ef082b45ec32df0ad9c13ac726
emilianscheel <emilian-scheel@t-online.de>
2022-05-19 18:54:56 +0200

    v2.6
    - adjusted diamat-mongodb.yml
    - adjusted mongo-init.sh

--- mongo-init.sh
+++ mongo-init.sh
@@ -3,28 +3,6 @@
 echo "########### Creating collections ###########"
 
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-    let MONGO_INITDB_ROOT_USERNAME = '$MONGO_INITDB_ROOT_USERNAME';
-    let MONGO_INITDB_ROOT_PASSWORD = '$MONGO_INITDB_ROOT_PASSWORD';
-    let MONGO_USERNAME = '$MONGO_USERNAME';
-    let MONGO_PASSWORD = '$MONGO_PASSWORD';
-      
-    // Erstelle den Nutzer
-    dbAdmin = db.getSiblingDB("admin");
-    dbAdmin.createUser({
-      user: MONGO_USERNAME,
-      pwd: MONGO_PASSWORD,
-      roles: [{ role: "userAdminAnyDatabase", db: "admin" }],
-      mechanisms: ["SCRAM-SHA-1"],
-    });
-
-    // Authentifiziere mit dem erstellen Nutzer
-    dbAdmin.auth({
-      user: MONGO_USERNAME,
-      pwd: MONGO_PASSWORD,
-      mechanisms: ["SCRAM-SHA-1"],
-      digestPassword: true,
-    });
-
     // Erstelle die Collections
     db = new Mongo().getDB("diamat-db");
     db.createCollection('begriffe', { capped: false });
